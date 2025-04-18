430b418b56c9723610bce470185eb10c119f220e
Julien Lesieur <lesieurj@proton.me>
2025-04-18 17:50:21 +0200

    chore: align all file new svelte config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from "drizzle-kit";
 
 dotenv.config();
 
+if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
+
 export default defineConfig({
 	dialect: "postgresql",
 	schema: "./src/lib/server/db",
 	dbCredentials: {
-		url: process.env.DATABASE_URL!,
+		url: process.env.DATABASE_URL,
 	},
 	verbose: true,
 	strict: true,
