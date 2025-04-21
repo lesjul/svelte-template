5739be830fa14f4e7070edc30085cdc95a53b3ff
Julien Lesieur <lesieurj@proton.me>
2025-04-21 11:12:09 +0200

    chore: update lefthook.yml config

--- src/routes/page.svelte.test.ts
+++ src/routes/page.svelte.test.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from "vitest";
-import { render } from '@testing-library/svelte';
+import { render } from "@testing-library/svelte";
 import Page from "./+page.svelte";
 
 describe("/+page.svelte", () => {
